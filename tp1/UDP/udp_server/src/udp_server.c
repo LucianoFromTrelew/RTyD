8b6fb6737c945a84e703ef5d480d9aae0b7cd4d9
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-03-27 11:59:58 -0300

    Terminar funcionalidad en el servidor UDP

--- tp1/UDP/udp_server/src/udp_server.c
+++ tp1/UDP/udp_server/src/udp_server.c
@@ -1,67 +1,62 @@
 #include<stdio.h>
 #include<strings.h> 
+#include<string.h> 
 #include<stdlib.h>
 #include<unistd.h>
 #include<arpa/inet.h>
 #include<sys/socket.h>
  
-#define BUF_SIZE 512  //Max length of buffer
-#define PORTNUMBER 12345   //The port on which to listen for incoming data
- 
-void die(char *_socket)
-{
-    perror(_socket);
-    exit(1);
-}
+#define BUF_SIZE 1024
+#define PORTNUMBER 12345
  
 int main(void)
 {
-    struct sockaddr_in direcc, si_other;
+    struct sockaddr_in direcc, client;
      
-    int _socket, slen = sizeof(si_other) , recv_len;
+    int _socket, slen = sizeof(client) , recv_len;
     char buf[BUF_SIZE];
      
-    //create a UDP socket
+    //creamos el socket UDP
     if ((_socket=socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
-        die("socket");
+        perror("*** ERROR AL CREAR EL SOCKET ***");
+        return 1;
     }
-     
-    // zero out the structure
+    /*limpiamos la estructura de datos*/
     bzero((char *) &direcc, sizeof(direcc));
-    /*memset((char *) &direcc, 0, sizeof(direcc));*/
      
     direcc.sin_family = AF_INET;
     direcc.sin_port = htons(PORTNUMBER);
     direcc.sin_addr.s_addr = htonl(INADDR_ANY);
      
-    //bind socket to port
+    //asociamos el socket a una dirección y un puerto
     if( bind(_socket , (struct sockaddr*)&direcc, sizeof(direcc) ) == -1)
     {
-        die("bind");
+        perror("*** ERROR AL ASOCIAR EL SOCKET ***");
+        return 1;
     }
      
-    //keep listening for data
+    //esperamos datos...
     while(1)
     {
-        printf("Waiting for data...");
-        fflush(stdout);
-         
-        //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(_socket, buf, BUF_SIZE, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        //recibimos datos
+        if ((recv_len = recvfrom(_socket, buf, BUF_SIZE, 0, (struct sockaddr *) &client, (socklen_t *) &slen)) == -1)
         {
-            die("recvfrom()");
+            perror("*** ERROR AL RECIBIR DATOS ***");
+            return 1;
         }
          
-        //print details of the client/peer and the data received
-        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Data: %s\n" , buf);
+        printf("%s:%d dice: %s", inet_ntoa(client.sin_addr), ntohs(client.sin_port), buf);
          
-        //now reply the client with the same data
-        if (sendto(_socket, buf, recv_len+1, 0, (struct sockaddr*) &si_other, slen) == -1)
+        //hacemos eco
+        if (sendto(_socket, buf, strlen(buf)+1, 0, (struct sockaddr*) &client, slen) == -1)
         {
-            die("sendto()");
+            perror("*** ERROR AL ENVIAR DATOS ***");
+            return 1;
         }
+
+        /*limpiamos el buffer de entrada*/
+        bzero(buf, BUF_SIZE);
     }
  
     close(_socket);
